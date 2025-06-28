189cc64803508fc19934e891f178cca98d1af025
TibixDev <tibix@fhs.sh>
2025-06-28 20:54:10 +0300

    feat: Add app icons, improve branding

--- src/renderer/views/About.vue
+++ src/renderer/views/About.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col items-center justify-center h-[calc(100vh-9rem)]">
-        <img class="w-48 contrast-75" src="/img/winboat_logo.png">
+        <img class="w-48" src="/img/winboat_logo.png">
         <h2 class="mb-0">WinBoat</h2>
         <p class="text-gray-500 text-sm">Windows for 🐧 penguins</p>
         <p class="text-gray-400 !mt-4">WinBoat Pre-Release Alpha {{ appVer }}</p>
