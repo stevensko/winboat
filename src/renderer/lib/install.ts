66bac2c1f9c13a6eacf4a8592e65880cad7abb21
David <142073269+KiLoBO@users.noreply.github.com>
2025-09-10 03:02:54 -0700

    Docker Image version pinning + Renovate Config (#90)
    * Create renovate.json5
    
    Should include only /src/renderer/lib/install.ts
    
    * Update install.ts
    
    * Update to use regex manager.
    
    * Set previous container version for testing.
    
    * chore(deps): update ghcr.io/dockur/windows docker tag to v4.35
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- src/renderer/lib/install.ts
+++ src/renderer/lib/install.ts
@@ -21,7 +21,7 @@ const defaultCompose: ComposeConfig = {
     },
     "services": {
         "windows": {
-            "image": "ghcr.io/dockur/windows:latest",
+            "image": "ghcr.io/dockur/windows:4.35",
             "container_name": "WinBoat",
             "environment": {
                 "VERSION": "11",
